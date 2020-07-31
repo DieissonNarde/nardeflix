c3935976399d243122e2fd53364c92d619a8a13a
DieissonNarde <dieissonnarde@gmail.com>
2020-07-31 15:41:05 -0300

    commit da aula 04

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import './index.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video';
-import CadastroCategoria from './pages/cadastro/Categoria'
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 //Desafio master na descrição
-const Pagina404 = () => (<div>Página 404</div>)
+const Pagina404 = () => (<iframe class="page404" src="https://mariosouto.com/flappy-bird-devsoutinho/" width="340" height="600"></iframe>);
 
 ReactDOM.render(
   <BrowserRouter>
@@ -19,7 +19,5 @@ ReactDOM.render(
       <Route component={Pagina404} />
     </Switch>
   </BrowserRouter>,
-  
   document.getElementById('root')
-);
-
+)
